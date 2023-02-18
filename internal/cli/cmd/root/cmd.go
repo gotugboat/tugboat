6041f3f2d987e09123e117bfa4232060c8eb90df
Jordan Cartwright <12487178+Jordan-Cartwright@users.noreply.github.com>
2023-02-18 16:34:59 -0500

    load the configuration and initialize logging

--- internal/cli/cmd/root/cmd.go
+++ internal/cli/cmd/root/cmd.go
@@ -1,18 +1,40 @@
 package root
 
 import (
+	"os"
+	"tugboat/internal/config"
 	"tugboat/internal/pkg/flags"
+	"tugboat/internal/pkg/logging"
 	"tugboat/internal/version"
 
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 func NewRootCommand(globalFlags *flags.GlobalFlagGroup) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:           "tugboat",
-		Version:       version.GetFullVersionWithArch(),
-		Short:         "Build multi-arch images",
-		Long:          rootDescription,
+		Use:     "tugboat",
+		Version: version.GetFullVersionWithArch(),
+		Short:   "Build multi-arch images",
+		Long:    rootDescription,
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			// Load the config
+			configPath := viper.GetString(flags.ConfigFileFlag.ConfigName)
+			if err := config.LoadConfig(configPath); err != nil {
+				return err
+			}
+
+			globalOptions := globalFlags.ToOptions()
+
+			logging.Initialize(os.Stderr, globalOptions.Debug)
+
+			if globalOptions.DryRun {
+				log.Warn("Dry run in progress, nothing will be executed")
+			}
+
+			return nil
+		},
 		SilenceUsage:  true,
 		SilenceErrors: true,
 	}
