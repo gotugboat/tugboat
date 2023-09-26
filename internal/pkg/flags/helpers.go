bfa36bc13ae3a39b298c3b3a068721c2c4ecf528
Jordan Cartwright <12487178+Jordan-Cartwright@users.noreply.github.com>
2023-09-26 18:12:15 -0400

    bind env for flags that contain '-'

--- internal/pkg/flags/helpers.go
+++ internal/pkg/flags/helpers.go
@@ -65,6 +65,18 @@ func bind(cmd *cobra.Command, flag *Flag) error {
 		}
 	}
 
+	if strings.Contains(flag.ConfigName, "-") {
+		var str string
+		replace_chars := []string{"-", "."}
+
+		for _, char := range replace_chars {
+			str = strings.ReplaceAll(flag.ConfigName, char, "_")
+		}
+		str = strings.ToUpper(str)
+
+		viper.BindEnv(flag.ConfigName, str)
+	}
+
 	// Bind the yaml configs to an env var (i.e log.format -> LOG_FORMAT)
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 	viper.AutomaticEnv()
